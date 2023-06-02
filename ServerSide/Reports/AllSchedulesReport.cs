980dd4deaa0f9db1ad990cdde3392dbab042b200
Yassine Hamadou <yassinehamadou1@gmail.com>
2023-06-02 17:57:07 +0000

    new changes

---
+++ ServerSide/Reports/AllSchedulesReport.cs
@@ -0,0 +1,13 @@
+using System;
+using DevExpress.XtraReports.UI;
+
+namespace ServerSide.Reports
+{
+    public partial class AllSchedulesReport
+    {
+        public AllSchedulesReport()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ServerSide/Reports/New folder/AllMeteringReportt.cs
@@ -0,0 +1,13 @@
+using System;
+using DevExpress.XtraReports.UI;
+
+namespace EnP_Reports.Reports
+{
+    public partial class AppraisalPerformanceSummaryReport
+    {
+        public AppraisalPerformanceSummaryReport()
+        {
+            InitializeComponent();
+        }
+    }
+}
